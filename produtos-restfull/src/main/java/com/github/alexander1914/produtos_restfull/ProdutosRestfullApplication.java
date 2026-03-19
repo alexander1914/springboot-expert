f492e1ce411f64dd3ec31758f2ff354ca4815446
alexander1914 <alexander.oliveira99@gmail.com>
2026-03-19 20:06:35 +0000

    [Set up of project] - I had been creating the project RestFull today

---
+++ produtos-restfull/src/main/java/com/github/alexander1914/produtos_restfull/ProdutosRestfullApplication.java
@@ -0,0 +1,15 @@
+package com.github.alexander1914.produtos_restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosRestfullApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(ProdutosRestfullApplication.class, args);
+
+	}
+
+}
---
+++ produtos-restfull/src/test/java/com/github/alexander1914/produtos_restfull/ProdutosRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alexander1914.produtos_restfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
