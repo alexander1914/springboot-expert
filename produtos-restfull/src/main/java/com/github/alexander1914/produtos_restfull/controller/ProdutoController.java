d690085fad78b8bbc3466cddff518fbabab77f4a
alexander1914 <alexander.oliveira99@gmail.com>
2026-03-20 18:24:56 +0000

    [Settings JPA H2] - I had been creating mapping with H2 and controller today

---
+++ produtos-restfull/src/main/java/com/github/alexander1914/produtos_restfull/controller/ProdutoController.java
@@ -0,0 +1,18 @@
+package com.github.alexander1914.produtos_restfull.controller;
+
+import com.github.alexander1914.produtos_restfull.entity.Produto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto){
+        System.out.println("Produto recebido: " + produto);
+        return produto;
+    }
+}
